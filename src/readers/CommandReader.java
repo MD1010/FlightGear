eccd1f6034bc95a34fb77339a5c564d899149d0f
dor1633 <dor1633@gmail.com>
2020-04-08 15:32:25 +0300

    add spaces after special chars

--- src/readers/CommandReader.java
+++ src/readers/CommandReader.java
@@ -1,28 +1,20 @@
 package readers;
 
-import mappers.CommandMapper;
-import utils.ReadCommandFile;
-import utils.Consts;
 import utils.HelperFuncs;
 
 
 public class CommandReader {
+    public static String[] commands;
+
     public static String[] splitAndRemoveSpaces(String commandString) {
         commandString = commandString.trim();
         return commandString.split(" ");
     }
 
-    public static void parser() {
-        String[] fileData = ReadCommandFile.fileData;
-        for (int lineIndex = 0; lineIndex < fileData.length; lineIndex++) {
-           // String[] lineToExecute = CommandReader.trimCommand(fileData[lineIndex]);
-            //System.out.println(fileData[lineIndex]);
-            lineIndex = HelperFuncs.executeCommand(fileData[lineIndex], lineIndex);
+    public static void runCommands(String[] lines) {
+        commands = HelperFuncs.addSpaces(lines);
+        for (int lineIndex = 0; lineIndex < commands.length; lineIndex++) {
+            lineIndex = HelperFuncs.executeCommand(commands[lineIndex], lineIndex);
         }
-    }
-
-    public static String[] lexer() {
-        return FileReader.readFile(Consts.FILE_NAME);
-
     }
 }
--- src/test/MyInterpreter.java
+++ src/test/MyInterpreter.java
@@ -1,15 +1,13 @@
 package test;
 
 import readers.CommandReader;
-import utils.ReadCommandFile;
 
 public class MyInterpreter {
 
 	public static  int interpret(String[] lines){
 		// call your interpreter here
 		// FileReader.readXMLFile(VARIABLE_XML);
-		ReadCommandFile.fileData = lines;
-		CommandReader.parser();
+		CommandReader.runCommands(lines);
 //        VariableMapper.printSymbolMap();
 
 		//DataServerReader.openServerConnection();
